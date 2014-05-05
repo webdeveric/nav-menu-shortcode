59ed0aaedbfa7d18c97db204bbc3706207bedc3d
Eric King <eric@webdeveric.com>
2014-05-05 15:42:51 -0500

    Cleaned up CS

--- nav-menu-shortcode.php
+++ nav-menu-shortcode.php
@@ -7,29 +7,28 @@ Author: Eric King
 Author URI: http://webdeveric.com
 */
 
-function nav_menu_shortcode( $atts, $content = null, $code = '' ){
+function nav_menu_shortcode($atts, $content = null, $code = '')
+{
+    $defaults = array(
+        'theme_location' => null,
+        'menu'           => null,
+        'container'      => false,
+        'container_id'   => null,
+        'menu_class'     => 'menu',
+        'menu_id'        => null,
+        'before'         => '',
+        'after'          => '',
+        'link_before'    => '',
+        'link_after'     => '',
+        'items_wrap'     => '<ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
+        'depth'          => 0,
+        'walker'         => null
+    );
 
-	$defaults = array(
-		'theme_location'  => null,
-		'menu'            => null,
-		'container'       => false,
-		'container_id'    => null,
-		'menu_class'      => 'menu',
-		'menu_id'         => null,
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => null
-	);
+    $atts = shortcode_atts($defaults, $atts);
+    $atts['echo'] = false;
 
-	$atts = shortcode_atts( $defaults, $atts );
-	$atts['echo'] = false;
-	$menu = wp_nav_menu( $atts );
-	return $menu;
+    return wp_nav_menu($atts);
 }
-
 
 add_shortcode('navmenu', 'nav_menu_shortcode');
